647af756bba7cfb654158b871ce05481084175bf
Felipe Alves <felipe.daconceicao@jala.university>
2025-10-07 21:57:19 -0300

    add[user]: RestFull API

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+import { Usuario } from './usuario.entity';
 
-@Controller('usuario')
-export class UsuarioController {}
+@Controller('usuarios')
+export class UsuarioController {
+  constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Post()
+  async criar(@Body() usuario: Partial<Usuario>): Promise<Usuario> {
+    return this.usuarioService.criarUsuario(usuario);
+  }
+
+  @Get()
+  async listar(): Promise<Usuario[]> {
+    return this.usuarioService.listarUsuarios();
+  }
+
+  @Get(':id')
+  async buscarPorId(@Param('id') id: string): Promise<Usuario | null> {
+    return this.usuarioService.buscarPorId(id);
+  }
+
+  @Put(':id')
+  async editar(
+    @Param('id') id: string,
+    @Body() usuario: Partial<Usuario>,
+  ): Promise<Usuario | null> {
+    return this.usuarioService.editarUsuario(id, usuario);
+  }
+
+  @Delete(':id')
+  async excluir(@Param('id') id: string): Promise<void> {
+    return this.usuarioService.excluirUsuario(id);
+  }
+}
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from './usuario.entity';
 
 @Injectable()
-export class UsuarioService {}
+export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async criarUsuario(usuario: Partial<Usuario>): Promise<Usuario> {
+    const novoUsuario = this.usuarioRepository.create(usuario);
+    return this.usuarioRepository.save(novoUsuario);
+  }
+
+  async listarUsuarios(): Promise<Usuario[]> {
+    return this.usuarioRepository.find();
+  }
+
+  async buscarPorId(id: string): Promise<Usuario | null> {
+    return this.usuarioRepository.findOne({ where: { id } });
+  }
+
+  async editarUsuario(
+    id: string,
+    usuario: Partial<Usuario>,
+  ): Promise<Usuario | null> {
+    await this.usuarioRepository.update(id, usuario);
+    return this.usuarioRepository.findOne({ where: { id } });
+  }
+
+  async excluirUsuario(id: string): Promise<void> {
+    await this.usuarioRepository.delete(id);
+  }
+}
