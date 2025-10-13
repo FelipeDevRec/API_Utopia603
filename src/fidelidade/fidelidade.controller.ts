c144b624f3a74f9adc98ae9b668112cc6550c068
Felipe Alves <felipe.daconceicao@jala.university>
2025-10-13 11:50:51 -0300

    add[Fidelidade]: Controller

---
+++ src/fidelidade/fidelidade.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { FidelidadeService } from './fidelidade.service';
+import { Fidelidade } from './fidelidade.entity';
+
+@Controller('fidelidades')
+export class FidelidadeController {
+  constructor(private readonly fidelidadeService: FidelidadeService) {}
+
+  @Post()
+  async criar(@Body() data: Partial<Fidelidade>): Promise<Fidelidade> {
+    return this.fidelidadeService.criarFidelidade(data);
+  }
+
+  @Get()
+  async listar(): Promise<Fidelidade[]> {
+    return this.fidelidadeService.listarFidelidades();
+  }
+
+  @Get(':id')
+  async buscarPorId(@Param('id') id: number): Promise<Fidelidade | null> {
+    return this.fidelidadeService.buscarPorId(id);
+  }
+
+  @Put(':id')
+  async editar(
+    @Param('id') id: number,
+    @Body() data: Partial<Fidelidade>,
+  ): Promise<Fidelidade | null> {
+    return this.fidelidadeService.editarFidelidade(id, data);
+  }
+
+  @Delete(':id')
+  async excluir(@Param('id') id: number): Promise<void> {
+    return this.fidelidadeService.excluirFidelidade(id);
+  }
+}
